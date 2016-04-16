f9b3b463b9b782e143b634e83a7fe39e539b92d7
shumarvin <shumarvin@gmail.com>
2016-04-16 14:47:38 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/shuma_000/gauchosell/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.shuma_000.gauchosell;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
