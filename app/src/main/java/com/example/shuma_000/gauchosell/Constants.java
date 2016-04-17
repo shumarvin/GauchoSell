b943336f5f1afea3a5768d2dca4a7a71d79e628d
davzaman <davzaman@gmail.com>
2016-04-16 18:56:09 -0700

    Initial set up for firebase

---
+++ app/src/main/java/com/example/shuma_000/gauchosell/Constants.java
@@ -0,0 +1,12 @@
+package com.example.shuma_000.gauchosell;
+
+/**
+ * Created by dav on 4/16/16.
+ */
+public class Constants {
+
+    /**
+     * Constants for Firebase URL
+     */
+    public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+}
--- app/src/main/java/com/example/shuma_000/gauchosell/MainActivity.java
+++ app/src/main/java/com/example/shuma_000/gauchosell/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.shuma_000.gauchosell;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.firebase.client.Firebase;
 
-public class MainActivity extends AppCompatActivity {
+/*public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+}*/
+
+    /**
+     * Includes one-time initialization of Firebase related code
+     */
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        /* Initialize Firebase */
+            Firebase.setAndroidContext(this);
+        /* Enable disk persistence  */
+            Firebase.getDefaultConfig().setPersistenceEnabled(true);
+        }
+
+
 }
