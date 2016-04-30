46f9cfda2224a7b0751a4e9fa42af7eb38b3db43
shumarvin <shumarvin@gmail.com>
2016-04-30 16:11:01 -0700

    created skeleton UI for postItem

--- app/src/main/java/cs48/g05/bbc2016/gauchosell/PostItemActivity.java
+++ app/src/main/java/cs48/g05/bbc2016/gauchosell/PostItemActivity.java
@@ -2,12 +2,25 @@ package cs48.g05.bbc2016.gauchosell;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.firebase.client.Firebase;
 
 public class PostItemActivity extends Activity {
+    private Firebase firebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_item);
+        firebaseRef = new Firebase(Constants.FIREBASE_URL);
+
+        //Initialize drop-down category menu
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
