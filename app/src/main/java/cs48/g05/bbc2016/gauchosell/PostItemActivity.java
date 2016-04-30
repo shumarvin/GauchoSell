983841c3c8edabd633f8f0582a4d8b1ad5e023e6
shumarvin <shumarvin@gmail.com>
2016-04-30 14:49:04 -0700

    added PostItemActivity

---
+++ app/src/main/java/cs48/g05/bbc2016/gauchosell/PostItemActivity.java
@@ -0,0 +1,13 @@
+package cs48.g05.bbc2016.gauchosell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_item);
+    }
+}
