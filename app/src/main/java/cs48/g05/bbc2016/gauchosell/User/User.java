67b598712c9b68cc8a2740b0a034c04fa817852b
bryanwarren <icemanbry@yahoo.com>
2016-05-13 13:34:17 -0700

    Got PostItem to work

--- app/src/main/java/cs48/g05/bbc2016/gauchosell/User/User.java
+++ app/src/main/java/cs48/g05/bbc2016/gauchosell/User/User.java
@@ -1,12 +1,20 @@
 package cs48.g05.bbc2016.gauchosell.user;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.firebase.client.Firebase;
+
 import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import cs48.g05.bbc2016.gauchosell.feeds.FollowingFeed;
 import cs48.g05.bbc2016.gauchosell.feeds.MyBidsFeed;
 import cs48.g05.bbc2016.gauchosell.feeds.MyItemsFeed;
 import cs48.g05.bbc2016.gauchosell.item.Bid;
+import cs48.g05.bbc2016.gauchosell.util.Constants;
 import cs48.g05.bbc2016.gauchosell.util.EmbeddedImage;
 import cs48.g05.bbc2016.gauchosell.item.Item;
 import cs48.g05.bbc2016.gauchosell.item.ItemInformation;
@@ -33,8 +41,15 @@ public class User {
 
     public void setAccount(Account myAccount) { this.myAccount = myAccount; }
     public Account getAccount() { return myAccount; }
-    public boolean postItem(ItemInformation itemDescription, String saleStatus, UUID itemID, Bid[]bids){
-        return true; //TODO: Firebase reference
+    public boolean postItem(ItemInformation itemDescription){
+        //Creates an Item object and  puts it in the database
+        Firebase firebaseRef = new Firebase(Constants.FIREBASE_URL);
+        HashMap<String, Object> userItemMapping = new HashMap<String, Object>();
+        Item newItem = new Item(itemDescription);
+        HashMap<String, Object> newItemMap = (HashMap<String, Object>) new ObjectMapper().convertValue(newItem, Map.class);
+        userItemMapping.put("/"+ Constants.FIREBASE_LOCATION_ITEMS + "/" + itemDescription.getTitle(), newItemMap);
+        firebaseRef.updateChildren(userItemMapping);
+        return true;
     }
     public boolean bidItem(Timestamp time, String username, UUID itemID, double amount){
         return true;
